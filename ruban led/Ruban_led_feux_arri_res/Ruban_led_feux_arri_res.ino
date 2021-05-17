35260986b604c815a624addf760a447cfb8d2974
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-17 16:01:52 +0200

    Update Ruban_led_feux_arri_res.ino

--- ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
+++ ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
@@ -8,6 +8,10 @@ int red = 5;
 int green = 255;
 int blue = 150;
 
+int orange_red = 255;
+int orange_green = 50;
+int orange_blue = 0;
+
 Adafruit_NeoPixel strip (LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
@@ -37,24 +41,38 @@ void loop() {
   }
   if (digitalRead(3) == 0) {
     for (int i = 0; i <= (LED_COUNT-1)/2; i++) {
-      strip.setPixelColor((LED_COUNT-1)/2-i, 255, 50, 0);
+      strip.setPixelColor((LED_COUNT-1)/2-i, orange_red, orange_green, orange_blue);
       delay(200);
       strip.show();
     }
     delay(200);
   }
-  else if (digitalRead(4) == 0) { 
-    for (int i = round((LED_COUNT-1)/2+1); i <= (LED_COUNT-1); i++) {
-      strip.setPixelColor(i, 255, 50, 0);
-      delay(200);
-      strip.show();
+  else if (digitalRead(4) == 0) {
+    if (LED_COUNT%2 == 0) {
+      for (int i = round((LED_COUNT-1)/2+1); i <= (LED_COUNT-1); i++) {
+        strip.setPixelColor(i, orange_red, orange_green, orange_blue);
+        delay(200);
+        strip.show();
+      }
+    }
+    else {
+      for (int i = (LED_COUNT-1)/2; i <= (LED_COUNT-1); i++) {
+        strip.setPixelColor(i, orange_red, orange_green, orange_blue);
+        delay(200);
+        strip.show();
+      }
     }
     delay(200);
   }
   else if (digitalRead(5) == 0) { 
     for (int i = 0; i <= (LED_COUNT-1)/2; i++) {
       strip.setPixelColor((LED_COUNT-1)/2-i, 255, 50, 0);
-      strip.setPixelColor((LED_COUNT-1)/2+i, 255, 50, 0);
+      if (LED_COUNT%2 == 0) {
+        strip.setPixelColor((LED_COUNT-1)/2+i+1, orange_red, orange_green, orange_blue);
+      }
+      else {
+        strip.setPixelColor((LED_COUNT-1)/2+i, orange_red, orange_green, orange_blue);
+      }
       delay(200);
       strip.show();
     }
