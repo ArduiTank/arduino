3c8f564ad539cc21f1d7603fa683d6ca7ab7a41b
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-17 16:49:42 +0200

    Update Ruban_led_feux_arri_res.ino

--- ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
+++ ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
@@ -12,13 +12,15 @@ int orange_red = 255;
 int orange_green = 50;
 int orange_blue = 0;
 
+int interation = 0;
+
 Adafruit_NeoPixel strip (LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   // put your setup code here, to run once:
   strip.begin();
   strip.show();
-  strip.setBrightness(255);
+  strip.setBrightness(100);
 
   pinMode(3, INPUT); // Clignotant gauche
   pinMode(4, INPUT); // Clignotant droit
@@ -27,18 +29,26 @@ void setup() {
 }
 
 void loop() {
+  // Couleur si tank bouge
   if (digitalRead(6) == 0) {
     for (int i = 0; i <= (LED_COUNT-1); i++) {
       strip.setPixelColor(i, 255, 0, 0);
     }
     strip.show();
   }
+  // Couleur par défaut
   else {
     for (int i = 0; i <= (LED_COUNT-1); i++) {
       strip.setPixelColor(i, red, green, blue);
     }
     strip.show();
   }
+  if (digitalRead(3) == 0 or digitalRead(4) == 0 or digitalRead(5) == 0) {
+    if (millis()%200 == 0) {
+      interation += 1;
+    }
+  }
+  
   if (digitalRead(3) == 0) {
     for (int i = 0; i <= (LED_COUNT-1)/2; i++) {
       strip.setPixelColor((LED_COUNT-1)/2-i, orange_red, orange_green, orange_blue);
