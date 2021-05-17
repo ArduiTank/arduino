5e5709a53fc3815d7cb51d0af6b771126a76769c
Antonin De Breuck <antonindebreuck@gmail.com>
2021-05-17 15:27:59 +0200

    Create Ruban_led_feux_arri_res.ino

---
+++ ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
@@ -0,0 +1,63 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 2
+#define LED_COUNT 5
+
+// Default color
+int red = 5;
+int green = 255;
+int blue = 150;
+
+Adafruit_NeoPixel strip (LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();
+  strip.show();
+  strip.setBrightness(255);
+
+  pinMode(3, INPUT); // Clignotant gauche
+  pinMode(4, INPUT); // Clignotant droit
+  pinMode(5, INPUT); // Feu de détresse
+  pinMode(6, INPUT); // Avance
+}
+
+void loop() {
+  if (digitalRead(6) == 0) {
+    for (int i = 0; i <= (LED_COUNT-1); i++) {
+      strip.setPixelColor(i, 255, 0, 0);
+    }
+    strip.show();
+  }
+  else {
+    for (int i = 0; i <= (LED_COUNT-1); i++) {
+      strip.setPixelColor(i, red, green, blue);
+    }
+    strip.show();
+  }
+  if (digitalRead(3) == 0) {
+    for (int i = 0; i <= (LED_COUNT-1)/2; i++) {
+      strip.setPixelColor((LED_COUNT-1)/2-i, 255, 50, 0);
+      delay(200);
+      strip.show();
+    }
+    delay(200);
+  }
+  else if (digitalRead(4) == 0) { 
+    for (int i = (LED_COUNT-1)/2; i <= (LED_COUNT-1); i++) {
+      strip.setPixelColor(i, 255, 50, 0);
+      delay(200);
+      strip.show();
+    }
+    delay(200);
+  }
+  else if (digitalRead(5) == 0) { 
+    for (int i = 0; i <= (LED_COUNT-1)/2; i++) {
+      strip.setPixelColor((LED_COUNT-1)/2-i, 255, 50, 0);
+      strip.setPixelColor((LED_COUNT-1)/2+i, 255, 50, 0);
+      delay(200);
+      strip.show();
+    }
+    delay(200);
+  }
+}
