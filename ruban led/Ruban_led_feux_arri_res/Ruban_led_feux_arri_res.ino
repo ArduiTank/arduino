34ee81aa80f961b1d8891c587677d3d49eee4150
AntoDB <antonindebreuck@gmail.com>
2021-05-23 23:06:20 +0200

    Update Ruban_led_feux_arri_res.ino

--- ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
+++ ruban led/Ruban_led_feux_arri_res/Ruban_led_feux_arri_res.ino
@@ -14,6 +14,10 @@ int red = 5;
 int green = 255;
 int blue = 150;
 
+int brightness_red = 0;
+int brightness_green = 0;
+int brightness_blue = 0;
+
 // Warning color
 int orange_red = 255;
 int orange_green = 50;
@@ -24,6 +28,7 @@ int iteration = -1;
 int reset = 1;
 
 int old_status = 1;
+int flag_begin = 1;
 
 // Time gestion
 int now_time = 0;
@@ -36,7 +41,7 @@ void setup() {
   strip.begin();
   strip.show();
   strip.setBrightness(brightness);
-
+  
   pinMode(3, INPUT); // Clignotant gauche
   pinMode(4, INPUT); // Clignotant droit
   pinMode(5, INPUT); // Feu de détresse
@@ -46,74 +51,94 @@ void setup() {
 }
 
 void loop() {
-  now_time = millis()/200;
-  if (reset == 0 and (digitalRead(3) == 0 or digitalRead(4) == 0 or digitalRead(5) == 0)) {
-    if (now_time != previous_time) {
-      previous_time = now_time;
-      iteration += 1;
-      if (iteration >= (LED_COUNT_FRONT/2)+1) {
-        iteration = -1;
-        reset = 1;
+  if (flag_begin == 1) {
+    for (int i = 0; i <= 255; i++) {
+      if (i <= red) {
+        brightness_red = i;
       }
-      Serial.println(iteration);
+      if (i <= green) {
+        brightness_green = i;
+      }
+      if (i <= blue) {
+        brightness_blue = i;
+      }
+      for (int j = 0; j <= LED_COUNT; j++) {
+        strip.setPixelColor(j, brightness_red, brightness_green, brightness_blue);
+      }
+      delay(10);
     }
+    flag_begin = 0;
   }
-  // Couleur si tank bouge
-  else if (digitalRead(6) == 0) {
-    reset = 0;
-    for (int i = 0; i <= (LED_COUNT_FRONT-1); i++) {
-      strip.setPixelColor(i, 255, 255, 255);
-    }
-    for (int i = LED_COUNT_SIDE; i <= (LED_COUNT_BACK-1); i++) {
-      strip.setPixelColor(i, 255, 0, 0);
-    }
-    strip.show();
-  }
-  // Couleur par défaut
   else {
-    reset = 0;
-    for (int i = 0; i <= (LED_COUNT-1); i++) {
-      strip.setPixelColor(i, red, green, blue);
-    }
-    strip.show();
-  }
-
-  // Chenillard lampes arrière
-  if (iteration != -1) {
-    if (digitalRead(3) == 0) {
-      strip.setPixelColor(((LED_COUNT_FRONT-1)/2)-iteration, orange_red, orange_green, orange_blue);
-      if (LED_COUNT_FRONT%2 == 0) {
-        strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2+1)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+    now_time = millis()/200;
+    if (reset == 0 and (digitalRead(3) == 0 or digitalRead(4) == 0 or digitalRead(5) == 0)) {
+      if (now_time != previous_time) {
+        previous_time = now_time;
+        iteration += 1;
+        if (iteration >= (LED_COUNT_FRONT/2)+1) {
+          iteration = -1;
+          reset = 1;
+        }
+        Serial.println(iteration);
       }
-      else {
-        strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+    }
+    // Couleur si tank bouge
+    else if (digitalRead(6) == 0) {
+      reset = 0;
+      for (int i = 0; i <= (LED_COUNT_FRONT-1); i++) {
+        strip.setPixelColor(i, 255, 255, 255);
+      }
+      for (int i = LED_COUNT_SIDE; i <= (LED_COUNT_BACK-1); i++) {
+        strip.setPixelColor(i, 255, 0, 0);
       }
       strip.show();
     }
-
-    else if (digitalRead(4) == 0) {
-      if (LED_COUNT_FRONT%2 == 0) {
-        strip.setPixelColor(((LED_COUNT_FRONT-1)/2+1)+iteration, orange_red, orange_green, orange_blue);
+    // Couleur par défaut
+    else {
+      reset = 0;
+      for (int i = 0; i <= (LED_COUNT-1); i++) {
+        strip.setPixelColor(i, red, green, blue);
       }
-      else {
-        strip.setPixelColor(((LED_COUNT_FRONT-1)/2)+iteration, orange_red, orange_green, orange_blue);
-      }
-      strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE-iteration, orange_red, orange_green, orange_blue);
       strip.show();
     }
-
-    else if (digitalRead(5) == 0) {
-      strip.setPixelColor(((LED_COUNT_FRONT-1)/2)-iteration, orange_red, orange_green, orange_blue);
-      strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE-iteration, orange_red, orange_green, orange_blue);
-      if (LED_COUNT_FRONT%2 == 0) {
-        strip.setPixelColor(((LED_COUNT_FRONT-1)/2+1)+iteration, orange_red, orange_green, orange_blue);
-        strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2+1)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+  
+    // Chenillard lampes arrière
+    if (iteration != -1) {
+      if (digitalRead(3) == 0) {
+        strip.setPixelColor(((LED_COUNT_FRONT-1)/2)-iteration, orange_red, orange_green, orange_blue);
+        if (LED_COUNT_FRONT%2 == 0) {
+          strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2+1)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+        }
+        else {
+          strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+        }
+        strip.show();
       }
-      else {
-        strip.setPixelColor(((LED_COUNT_FRONT-1)/2)+iteration, orange_red, orange_green, orange_blue);
-        strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+  
+      else if (digitalRead(4) == 0) {
+        if (LED_COUNT_FRONT%2 == 0) {
+          strip.setPixelColor(((LED_COUNT_FRONT-1)/2+1)+iteration, orange_red, orange_green, orange_blue);
+        }
+        else {
+          strip.setPixelColor(((LED_COUNT_FRONT-1)/2)+iteration, orange_red, orange_green, orange_blue);
+        }
+        strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE-iteration, orange_red, orange_green, orange_blue);
+        strip.show();
       }
-      strip.show();
+  
+      else if (digitalRead(5) == 0) {
+        strip.setPixelColor(((LED_COUNT_FRONT-1)/2)-iteration, orange_red, orange_green, orange_blue);
+        strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE-iteration, orange_red, orange_green, orange_blue);
+        if (LED_COUNT_FRONT%2 == 0) {
+          strip.setPixelColor(((LED_COUNT_FRONT-1)/2+1)+iteration, orange_red, orange_green, orange_blue);
+          strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2+1)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+        }
+        else {
+          strip.setPixelColor(((LED_COUNT_FRONT-1)/2)+iteration, orange_red, orange_green, orange_blue);
+          strip.setPixelColor((((LED_COUNT_BACK-LED_COUNT_SIDE)-1)/2)+LED_COUNT_SIDE+iteration, orange_red, orange_green, orange_blue);
+        }
+        strip.show();
+      }
     }
   }
 }
